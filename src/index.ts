20eb3fee2e0e7615977cd86a3c516c2dab948bf0
José Augusto Megres de Oliveira <jose.megres@gmail.com>
2023-08-05 01:26:27 -0300

    feat: adding new error handler

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
+import 'express-async-errors'
 import 'dotenv/config'
-import './controllers'
 import express from 'express'
+import './controllers'
 import { appRoutes } from './routes'
+import { errorHandler } from './routes/middlewares/errorHandler'
 
 const port = process.env.PORT
 
 const app = express()
 
 app.use(appRoutes)
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Listening on ${port}`))
