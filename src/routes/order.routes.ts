4d24776db01a77519e749d6c42d2d4342b43c452
José Augusto Megres de Oliveira <jose.megres@gmail.com>
2023-08-05 01:27:06 -0300

    refactor: renaming routes

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -3,7 +3,7 @@ import { getOrderController, syncOrderController } from '../controllers'
 
 const order = Router()
 
-order.get('/order', async (req, res) => await getOrderController.handle(req, res))
-order.get('/order/sync', async (req, res) => await syncOrderController.handle(req, res))
+order.get('/orders', async (req, res) => await getOrderController.handle(req, res))
+order.get('/orders/sync', async (req, res) => await syncOrderController.handle(req, res))
 
 export { order }
--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -3,7 +3,7 @@ import { getProductController, syncProductController } from '../controllers'
 
 const product = Router()
 
-product.get('/product/sync', async (req, res) => await syncProductController.handle(req, res))
-product.get('/product', async (req, res) => await getProductController.handle(req, res))
+product.get('/products/sync', async (req, res) => await syncProductController.handle(req, res))
+product.get('/products', async (req, res) => await getProductController.handle(req, res))
 
 export { product }
